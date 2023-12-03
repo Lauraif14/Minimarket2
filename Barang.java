cb5731dfe15b2625c956f1fbdb2318b42029159f
Lauraif14 <145311957+Lauraif14@users.noreply.github.com>
2023-12-03 21:58:05 +0700

    Add files via upload

---
+++ Barang.java
@@ -0,0 +1,25 @@
+// Class Barang sebagai superclass
+    class Barang {
+        protected String namaBarang;
+        protected double hargaBarang;
+        protected int jumlahBarang;
+    
+        public Barang(String namaBarang, double hargaBarang, int jumlahBarang) {
+            this.namaBarang = namaBarang;
+            this.hargaBarang = hargaBarang;
+            this.jumlahBarang = jumlahBarang;
+        }
+    public String getNamaBarang() {
+        return namaBarang;
+    }
+
+    public double getHarga() {
+        return hargaBarang;
+    }
+
+    public int getJumlah() {
+        return jumlahBarang;
+    }
+}
+
+
